52fa7266586079142e75b2cbce4c85d91d41b844
JayminPatel18 <pateljaymin551@gmail.com>
2023-01-25 10:48:10 +0530

    add comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 
+// Text Widget
 void main() {
-  runApp(
-    Text(
-      'Flutter Crash Course',
-      textDirection: TextDirection.ltr,
-      style: TextStyle(fontSize: 50.0),
-    )
-  );
+  runApp(Text(
+    'Flutter Crash Course',
+    textDirection: TextDirection.ltr, // ltr - left to right in text Direction
+    style: TextStyle(fontSize: 50.0), // font-size
+  ));
 }
-
-
