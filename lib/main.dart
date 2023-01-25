a2b31beae969f43aaa0a805877bcb1ff7a468aeb
JayminPatel18 <pateljaymin551@gmail.com>
2023-01-25 17:07:20 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,17 @@ void main() {
     title: 'Flutter', // create tittle
     home: Container(
       // use container
+      decoration: BoxDecoration(color: Colors.pinkAccent),
       child: Center(
           // make element in center
           child: Text(
         // elements is text
         'Material App',
         textDirection: TextDirection.ltr,
+        style: TextStyle(
+            color: Colors.lightBlue, // change text color
+            decoration: TextDecoration
+                .none), // remove the decoration - remove underline
       )),
     ),
   ));
