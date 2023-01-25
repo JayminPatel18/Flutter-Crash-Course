5663f18722926257f26dc0b945e27c72b3c12e30
JayminPatel18 <pateljaymin551@gmail.com>
2023-01-25 11:11:49 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,10 @@ void main() {
     children: [
       // it initalize the children list. it can store like array
       Expanded(
-          child: Text(
+        child: Text(
         'one',
-        textDirection: TextDirection
-            .ltr, // sometimes text are overflow then we can set using expanded
-        style: TextStyle(
-            fontSize:
-                50.0), // here text 'one' & 'two' are set by require spaces
+        textDirection: TextDirection.ltr,    // sometimes text are overflow then we can set using expanded
+        style: TextStyle(fontSize: 50.0),    // here text 'one' & 'two' are set by require spaces
       )),
       Expanded(
           child: Text(
