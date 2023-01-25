2786a85c02fd751ae09697109d2233daabd9b75a
JayminPatel18 <pateljaymin551@gmail.com>
2023-01-25 10:38:07 +0530

    First Simple Program

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Text(
+      'Flutter Crash Course',
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 50.0),
+    )
+  );
+}
+
+
