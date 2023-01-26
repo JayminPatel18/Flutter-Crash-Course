2ffd6e31ea682e343b63eb8bb18cae641b39a203
JayminPatel18 <pateljaymin551@gmail.com>
2023-01-26 15:49:34 +0530

    image widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// Scalfold Widget
+// Image Widget
 void main() {
   runApp(MaterialApp(
     title: 'Flutter', // create tittle
@@ -16,17 +16,13 @@ void main() {
        
         backgroundColor: Colors.cyan,       // set background color
        
-        body: Center(
-          child: Text(
-            'Material App',
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              color: Colors.black,              // set text color
-              decoration: TextDecoration.none,
-              fontSize: 40,
-            ),
-            )
-        )
-      ,)
+        //body:Image(image: AssetImage('images/car1.jpg'), fit: BoxFit.cover,)      // set images in body
+
+        body: Image(image: NetworkImage(
+          'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fkumarraunak610%2Fcar-on-road-photography%2F&psig=AOvVaw2SOq7oTbmuZH1cQzeiUlE8&ust=1674814551424000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNDxqsaA5fwCFQAAAAAdAAAAABAJ'), 
+          fit: BoxFit.fill,), 
+         // set image from browser
+
+        ),
   ));
 }
