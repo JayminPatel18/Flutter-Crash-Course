6623c591ea693c35bd3880bbb5a16e3651188008
JayminPatel18 <pateljaymin551@gmail.com>
2023-01-26 17:44:31 +0530

    grid widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
 
-// Image Widget
+// grid Widget
 void main() {
   runApp(MaterialApp(
     title: 'Flutter', // create tittle
     home: Scaffold(
-       
-        appBar: AppBar(             // set head
+        appBar: AppBar(
+          // set head
           title: Text(
             'My Flutter App',
             style: TextStyle(color: Colors.black),
-          ), 
-          backgroundColor: Colors.pinkAccent,   
+          ),
+          backgroundColor: Colors.pinkAccent,
         ),
-       
-        backgroundColor: Colors.cyan,       // set background color
-       
-        //body:Image(image: AssetImage('images/car1.jpg'), fit: BoxFit.cover,)      // set images in body
+        backgroundColor: Colors.cyan, // set background color
 
-        body: Image(image: NetworkImage(
-          'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fkumarraunak610%2Fcar-on-road-photography%2F&psig=AOvVaw2SOq7oTbmuZH1cQzeiUlE8&ust=1674814551424000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNDxqsaA5fwCFQAAAAAdAAAAABAJ'), 
-          fit: BoxFit.fill,), 
-         // set image from browser
+        body: GridView.extent(
+          // we can use grid view to adjust elements or image as own choice
+
+          maxCrossAxisExtent:
+              300, // pixel according to change the space and adjust the image
+          mainAxisSpacing: 50, // space between the top bottom
+          crossAxisSpacing: 70, // space between the left right
+          children: [
+            Image(image: AssetImage('images/cars/car1.jpg')),
+            Image(image: AssetImage('images/cars/car2.jpg')),
+            Image(image: AssetImage('images/cars/car3.jpg')),
+            Image(image: AssetImage('images/cars/car4.jpg')),
+            Image(image: AssetImage('images/cars/car5.jpg')),
+            Image(image: AssetImage('images/cars/car6.jpg')),
+          ],
+        )
+
+        // set image from browser
 
         ),
   ));
