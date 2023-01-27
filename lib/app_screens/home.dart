bc3856da6b33adeabf4c8210f829b689dc3cebac
JayminPatel18 <pateljaymin551@gmail.com>
2023-01-27 17:54:43 +0530

    registration ui

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -6,31 +6,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String name = "";
+  final _formKey = GlobalKey<FormState>();
+
+  var name = "";
+  var email = "";
+  var password = "";
+
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(children: [
-        TextField(
-          // onSubmitted: (value) {          // change the name
-          //   setState(() {
-          //     name = value;
-          //   });
-          // },
-
-          onChanged: (value) {
-            // print the text same time to type
-            setState(() {
-              name = value;
-            });
-          },
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 40, horizontal: 30),
+        child: ListView(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Name : ', labelStyle: TextStyle(fontSize: 20)),
+              controller: nameController,
+              validator: (value) {
+                if (value == null) return 'Please Enter Name';
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Email : ', labelStyle: TextStyle(fontSize: 20)),
+              controller: emailController,
+              validator: (value) {
+                if (value == null)
+                  return 'Please Enter Email';
+                else if (!value.contains('@'))
+                  return 'Please enter valid email';
+              },
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  labelText: 'Password : ',
+                  labelStyle: TextStyle(fontSize: 20)),
+              controller: passwordController,
+              validator: (value) {
+                if (value == null) return 'Please Enter Password';
+              },
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    setState(() {
+                      name = nameController.text;
+                      email = emailController.text;
+                      password = passwordController.text;
+                    });
+                  }
+                },
+                child: Text('Submit')),
+            Text('Name : $name'),
+            Text('Email : $email'),
+            Text('Password : $password'),
+          ],
         ),
-        Text(
-          name,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(fontSize: 30),
-        )
-      ]),
+      ),
     );
   }
 }
